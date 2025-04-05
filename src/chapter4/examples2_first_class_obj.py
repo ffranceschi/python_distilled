aa14d0974e99bfc4c794a28336a97e0ce130f6d6
Fernando Franceschi <fernando.franceschi@sumus.com.br>
2025-04-05 16:39:33 -0300

    capitulo 4

---
+++ src/chapter4/examples2_first_class_obj.py
@@ -0,0 +1,19 @@
+# __add__(self, other)
+# __sub__(self, other)
+# __mul__(self, other)
+# __and__(self, other)
+# __or__(self, other)
+
+class Pessoa:
+    def __init__(self, idade):
+        self.idade = idade
+
+    def __add__(self, pessoa):
+        return Pessoa(self.idade + pessoa.idade)
+
+p1 : Pessoa = Pessoa(20)
+p2 : Pessoa = Pessoa(45)
+
+p3 = p1 + p2
+print(f"Idade da nova pessoa: {p3.idade}")
+
